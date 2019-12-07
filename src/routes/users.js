3bfcf82f86e158700f4829479a9d1fc466dfa695
Lovisgod <olifedayo94@gmail.com>
2019-12-07 07:35:33 +0100

    CH/get for verify

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/users/signup', authcontroller.signUp);
 router.post('/users/login', authcontroller.signIn);
 router.get('/users/profile', auth, authcontroller.me);
-router.put('/auth/verify', authcontroller.verify);
+router.get('/auth/verify', authcontroller.verify);
 router.put('/users/get-new-email', authcontroller.getNewEmailTOken);
 router.put('/users/update-profile', auth, authcontroller.updateProfile);
 router.put('/users/reset-pass', authcontroller.resetPassword);
