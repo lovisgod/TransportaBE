bb1a466207072df1301d47cc1ec1a8d271fbf42c
lovisgod <olifedayo94@gmail.com>
2021-01-08 19:32:51 +0100

    bg:fix tsc issues

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,5 @@ export const database = new Sequelize({
    username:"ccb5c8429f476bcfd70f35725d5e81c0c3ae60eddcf632f58c97f31653256810",
    host: "ec2-18-235-107-171.compute-1.amazonaws.com",
    port: 5432,
-   dialect: "postgres",
-   ssl: true
+   dialect: "postgres"
 });
