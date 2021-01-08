6806be8aabd6020647ac3183621bf475b606bda5
lovisgod <olifedayo94@gmail.com>
2021-01-08 19:15:22 +0100

    bg:fix tsc issues'

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
 
 export const database = new Sequelize({
-  database: "wellness_db",
-  dialect: "sqlite",
-  storage: ":memory:",
+  database: "d59lugjjuo4sb4",
+   password: "sjkbdomwzsbeco",
+   username:"ccb5c8429f476bcfd70f35725d5e81c0c3ae60eddcf632f58c97f31653256810",
+   host: "ec2-18-235-107-171.compute-1.amazonaws.com",
+   port: 5432
 });
