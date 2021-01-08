a41c79c1945e6b2d4873bb1f732ac8f0e4b7182b
lovisgod <olifedayo94@gmail.com>
2021-01-08 19:35:24 +0100

    bg:fix tsc issues

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,8 @@ import { Sequelize } from "sequelize";
 
 export const database = new Sequelize({
   database: "d59lugjjuo4sb4",
-   password: "sjkbdomwzsbeco",
-   username:"ccb5c8429f476bcfd70f35725d5e81c0c3ae60eddcf632f58c97f31653256810",
+   password: "ccb5c8429f476bcfd70f35725d5e81c0c3ae60eddcf632f58c97f31653256810",
+   username:"sjkbdomwzsbeco",
    host: "ec2-18-235-107-171.compute-1.amazonaws.com",
    port: 5432,
    dialect: "postgres"
